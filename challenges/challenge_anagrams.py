c30c6db9213ff7d07f8c07b044edf5abeff2fea1
Thiago Lira <thiago.slira@outlook.com>
2023-06-01 19:47:55 -0300

    Função para ordenar a string refatorada

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,38 @@
+def merge_sort(string):
+    if len(string) <= 1:
+        return string
+
+    mid = len(string) // 2
+    left = merge_sort(string[:mid])
+    right = merge_sort(string[mid:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    merged = []
+    left_index, right_index = 0, 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+
+    merged.extend(left[left_index:])
+    merged.extend(right[right_index:])
+
+    return "".join(merged)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    return ""
+
+
+word1 = "batata"
+word1_in_list = list(word1)
+word2 = "cenoura"
+
+print(merge_sort(word1))
