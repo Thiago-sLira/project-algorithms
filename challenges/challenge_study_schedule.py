81a21cd026fbbc5d2754319bf71d4d51c72c92e0
Thiago Lira <thiago.slira@outlook.com>
2023-06-01 17:38:31 -0300

    Requisito 1 concluído

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,23 +1,20 @@
-period = [(2, 2), (1, 2), (2), (1, 5), (4, 5), (4, 5)]
-
-
 def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
 
     students_online = 0
     for entry, exit in permanence_period:
-        if entry is None or exit is None:
+        if not isinstance(entry, int) or not isinstance(exit, int):
             return None
-        else:
-            student_range = list(range(entry, exit + 1))
-            if target_time in student_range:
-                students_online += 1
+        # student_range = list(range(entry, exit + 1))
+        if entry <= target_time <= exit:
+            students_online += 1
 
     return students_online
 
 
-print(study_schedule(period, 3))
+# period = [(2, 2), (1, 2), (2), (1, 5), (4, 5), (4, 5)]
+# print(study_schedule(period, 3))
 
 # entry, exit = period[3]
 # print(list(range(entry, exit + 1)))
