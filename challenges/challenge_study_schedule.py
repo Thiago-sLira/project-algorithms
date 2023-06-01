00c412ea4ebd2b2a58a04e0f89231d9d45b052a0
Thiago Lira <thiago.slira@outlook.com>
2023-06-01 17:30:42 -0300

    Função funcionando, faltando verificar se receber um valor inválido no permanence_period

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,23 @@
-period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+period = [(2, 2), (1, 2), (2), (1, 5), (4, 5), (4, 5)]
 
 
 def study_schedule(permanence_period, target_time):
+    if target_time is None:
+        return None
+
+    students_online = 0
     for entry, exit in permanence_period:
-        student_range = list(range(entry, exit + 1))
-        print(student_range)
+        if entry is None or exit is None:
+            return None
+        else:
+            student_range = list(range(entry, exit + 1))
+            if target_time in student_range:
+                students_online += 1
+
+    return students_online
 
 
-(study_schedule(period, 5))
+print(study_schedule(period, 3))
 
-entry, exit = period[3]
-print(list(range(entry, exit + 1)))
+# entry, exit = period[3]
+# print(list(range(entry, exit + 1)))
