cc1e2b13006aad777f2e25977b7577602f177ae2
Thiago Lira <thiago.slira@outlook.com>
2023-06-01 18:38:31 -0300

    Asserções para casos de erros

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,17 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    # Caso de sucesso
+    with pytest.raises(TypeError) as erroKey:
+        encrypt_message("menssagem", None)
+    assert str(erroKey.value) == "tipo inválido para key"
+
+    with pytest.raises(TypeError) as erroMessage:
+        encrypt_message(None, 2)
+    assert str(erroMessage.value) == "tipo inválido para message"
+
+    assert encrypt_message("menssagem", 10) == "megassnem"
+
+    assert encrypt_message("menssagem", 2) == "megassn_em"
