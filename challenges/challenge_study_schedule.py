b054ffa4ce5cab253c65a004b80e659931766c38
Thiago Lira <thiago.slira@outlook.com>
2023-06-01 17:13:53 -0300

    Criado um range para cada aluno indicando todos os horários presente na plataforma

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,5 +2,12 @@ period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
 
 
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    for entry, exit in permanence_period:
+        student_range = list(range(entry, exit + 1))
+        print(student_range)
+
+
+(study_schedule(period, 5))
+
+entry, exit = period[3]
+print(list(range(entry, exit + 1)))
