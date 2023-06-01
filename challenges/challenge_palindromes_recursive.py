1ea525fe5a92f90c6baef2c73b4d408beeda8571
Thiago Lira <thiago.slira@outlook.com>
2023-06-01 19:24:22 -0300

    Função de forma recursiva concluída

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,17 +1,20 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if word is None:
+    if len(word) == 0:
         return False
 
-    if low_index == high_index:
+    if low_index >= high_index:  # Condição de parada
         return True
 
-    if "haushausha":
+    if (
+        word[low_index] != word[high_index]
+    ):  # Verifica os caracteres da palavra
         return False
 
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
 
-print(is_palindrome_recursive("socos", "s", "s"))
+word = "atata"
+print(is_palindrome_recursive(word, 0, len(word) - 1))
 # reversed_list = list("thiago")
 # reversed_list.reverse()
 # print(reversed_list)
