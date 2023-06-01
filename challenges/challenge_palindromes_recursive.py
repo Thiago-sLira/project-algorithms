8a7ad9cebed8e7b155d19400bdec2a7cf3df35fa
Thiago Lira <thiago.slira@outlook.com>
2023-06-01 19:12:15 -0300

    Transformando a função em recursiva

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,15 +1,17 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if word is None:
         return False
-    else:
-        string_list = list(word)
-        string_list.reverse()
-        new_string = "".join(string_list)
-        return new_string[0] == low_index and new_string[-1] == high_index
-    return is_palindrome_recursive()
+
+    if low_index == high_index:
+        return True
+
+    if "haushausha":
+        return False
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
 
-print(is_palindrome_recursive('socos', 's', 's'))
+print(is_palindrome_recursive("socos", "s", "s"))
 # reversed_list = list("thiago")
 # reversed_list.reverse()
 # print(reversed_list)
