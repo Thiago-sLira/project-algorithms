a394d685c303a63e66625b8c94a2405bb94d282b
Thiago Lira <thiago.slira@outlook.com>
2023-06-02 15:20:14 -0300

    Requisito 5 concluído

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,7 +1,16 @@
 def find_duplicate(nums):
-    if not nums or type(nums) != list:
+    if not nums or len(nums) == 1:
         return False
+    # return True
+    num_duplicate = set()
+    for num in nums:
+        if type(num) == str or num < 1:
+            return False
+        if num in num_duplicate:
+            return num
+        num_duplicate.add(num)
+    return False
 
 
-print(find_duplicate([]))
-print(type([]) == list)
+print(find_duplicate([1, 2]))
+# print(type("string") == str)
