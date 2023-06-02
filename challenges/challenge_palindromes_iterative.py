1cdc4612a35cb468c8ead237bb98775a6a6bc5d2
Thiago Lira <thiago.slira@outlook.com>
2023-06-02 15:26:52 -0300

    Requisito 6 concluído

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word is None or len(word) == 0:
+        return False
+    else:
+        string_list = list(word)
+        string_list.reverse()
+        new_string = "".join(string_list)
+        return new_string == word
